208c82312cfb21db6dca50b723fd5ff544adeb08
Nguyễn Huỳnh Duy Tân <21110852@student.hcmute.edu.vn>
2023-11-01 07:59:19 +0700

    The base code and first update
    Created with winform c#

---
+++ NumberGuessing/NumberGuessing/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NumberGuessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(260, 116);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(269, 22);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Enter your guess here";
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtInput.Click += new System.EventHandler(this.txtInput_Click);
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Location = new System.Drawing.Point(12, 9);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(132, 16);
+            this.lblGuess.TabIndex = 1;
+            this.lblGuess.Text = "You guessed 0 times";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(260, 203);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(95, 49);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(434, 203);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(95, 49);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check!";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "My guess is:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
